4471d07f1474fe1e61aebaeb3e93a795bd8a0ecb
muhammadaqib1994 <108095341+muhammadaqib1994@users.noreply.github.com>
2022-09-21 01:46:58 -0700

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -7,3 +7,15 @@ variable "eks_cluster_name" {
   type = string
   default = "1.20"
  }
+
+variable "region" {
+  type = string
+  default     = "us-west-2"
+  description = "AWS region"
+}
+
+variable "vpc_name" {
+  type = string
+  default     = "springboot-vpc"
+  description = "AWS region"
+}
